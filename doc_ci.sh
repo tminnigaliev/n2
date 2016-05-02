f29da26769d39ecc4675d57d3a213ab82ebbb8b1
tminnigaliev <t.minnigaliev@mail.ru>
2016-05-03 01:58:27 +0300

    fixing doc

--- doc_ci.sh
+++ doc_ci.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd gh-pages/n2/doxygen/
-git rm -rf *
-cp -r ../../code/n2/doc/html ./
-git add *
-git commit * -m "replacing old docs with new"
+git rm -rf html
+cp -r ../../../code/n2/doc/html ./
+git add html
+git commit html -m "replacing old docs with new"
 
 git push origin gh-pages
 
