2998011382ae7eafbc35eac49d0340098b5601e7
tminnigaliev <t.minnigaliev@mail.ru>
2016-05-03 00:22:59 +0300

    splitting doc_gen to doc_gen and doc_ci

--- doc_gen.sh
+++ doc_gen.sh
@@ -6,12 +6,4 @@ cp ../gh-pages/n2/Doxyfile .
 cp -r ../gh-pages/n2/src .
 doxygen Doxyfile
 
-cd ../gh-pages/n2
-git rm -rf *
-cp -r ../../tmp_doc/* .
-git add *
-git commit -m "replacing old docs with new"
-
-git push origin gh-pages
-
 
