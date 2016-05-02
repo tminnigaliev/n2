4da5bc7aafad968b624c158a7a552a8e65787abb
tminnigaliev <t.minnigaliev@mail.ru>
2016-05-03 00:38:48 +0300

    fix, added * to commit

--- doc_ci.sh
+++ doc_ci.sh
@@ -4,7 +4,7 @@ cd gh-pages/n2
 git rm -rf *
 cp -r ../../tmp_doc/* .
 git add *
-git commit -m "replacing old docs with new"
+git commit * -m "replacing old docs with new"
 
 git push origin gh-pages
 
